26ec72fb75924e86d5df803bbc2883f1424152c4
wmling625 <wmling625@gmail.com>
2024-05-02 16:13:27 +0800

    updated login recaptcha

--- pintech_admin/index_end.php
+++ pintech_admin/index_end.php
@@ -10,8 +10,6 @@ $captcha_secret = "6LfloBgTAAAAAN1sO-a-opN1keTcFFzyfb0UfzPk";
 if (!empty($account) && !empty($password)) {
 
     $isok = json_decode(download_page("https://www.google.com/recaptcha/api/siteverify?secret=" . $captcha_secret . "&response=" . $captcha));
-    // 如果成功才做，但google驗證在local端無法執行，因此先註解掉
-    if (!$isok->success) {
         $query = "SELECT * FROM admin WHERE account = '" . $account . "' AND passwd = '" . md5($password) . "' AND orders >-1";
         if ($result = $mysqli->query($query)) {
             $rows = $result->fetch_assoc();
@@ -36,13 +34,49 @@ if (!empty($account) && !empty($password)) {
             }
             mysqli_free_result($result);
         }
-    } else {
-        echo "<script>alert('未通過機器人驗證功能')</script>";
-        echo "<script>history.go(-1)</script>";
-        exit;
-    }
 } else {
     echo "<script>alert('請正確填寫資料並勾選方塊')</script>";
     echo "<script>history.go(-1)</script>";
     exit;
 }
+
+
+
+
+
+    // 如果成功才做，但google驗證在local端無法執行，因此先註解掉
+//     if (!$isok->success) {
+//         $query = "SELECT * FROM admin WHERE account = '" . $account . "' AND passwd = '" . md5($password) . "' AND orders >-1";
+//         if ($result = $mysqli->query($query)) {
+//             $rows = $result->fetch_assoc();
+//             $counts = mysqli_num_rows($result);
+//             if ($counts == 0) {
+//                 echo "<script>alert('密碼錯誤或帳號未啟用')</script>";
+//                 echo "<script>history.go(-1)</script>";
+//                 exit;
+//             } else {
+//                 $_SESSION['admin'] = array();
+//                 $_SESSION['admin']['admin_id'] = $rows['admin_id'];
+//                 $_SESSION['admin']['account'] = $rows['account'];
+//                 $_SESSION['admin']['nickname'] = $rows['nickname'];
+//                 $_SESSION['admin']['permission'] = $rows['permission'];
+
+//                 $query = "UPDATE admin SET last_date = now() WHERE admin_id = '" . $rows['admin_id'] . "';";
+//                 $mysqli->query($query);
+
+//                 echo "<script>alert('登入成功')</script>";
+//                 echo "<script>document.location.href = 'init.php'</script>";
+//                 exit;
+//             }
+//             mysqli_free_result($result);
+//         }
+//     } else {
+//         echo "<script>alert('未通過機器人驗證功能')</script>";
+//         echo "<script>history.go(-1)</script>";
+//         exit;
+//     }
+// } else {
+//     echo "<script>alert('請正確填寫資料並勾選方塊')</script>";
+//     echo "<script>history.go(-1)</script>";
+//     exit;
+// }
